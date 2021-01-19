414c66cac52e8a01af0f20fa418f8717e6a3e2f5
James Daniels <james.daniels@xceptor.com>
2021-01-19 16:25:33 +0000

    Working sample as outlined in this tutorial

---
+++ dotnet/dependency-injection/console/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace DotNet.DependecyInjection.ConsoleApp
+{
+    class MyClass
+    {
+        private readonly ILogger _logger;
+
+        public string Message { get; set; }
+
+        public MyClass(ILogger<MyClass> logger)
+        {
+            _logger = logger;
+        }
+
+        public void WriteMessage()
+        {
+            Message = $"Main Run {DateTime.UtcNow.ToLongTimeString()}";
+            _logger.LogInformation(Message);
+            Console.WriteLine("Finsihed WriteMessage");
+        }
+    }
+}
---
+++ dotnet/dependency-injection/console/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace DotNet.DependecyInjection.ConsoleApp
+{
+    public class Startup
+    {
+        public Startup()
+        {
+
+        }
+        public void ConfigureServices()
+        {
+            
+        }
+    }
+}
